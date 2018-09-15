b385b2fa990744d476ad7d11138b8f8ec3e31a50
Bryan <2015081611@ust-ics.mygbiz.com>
2018-09-15 21:46:07 +0800

    Initial commit

---
+++ app/src/main/java/bryan/galon/com/galonbryanlab3/GalonBryanLab3.java
@@ -0,0 +1,13 @@
+package bryan.galon.com.galonbryanlab3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GalonBryanLab3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_galon_bryan_lab3);
+    }
+}
